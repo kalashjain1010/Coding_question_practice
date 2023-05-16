0cd641f14b4032df22de04223c57eeebbb6701f1
kalash jain <kalashjain54@gmail.com>
2023-05-16 23:35:27 +0530

    tcs ques 1

--- tcs/sumOfDiagonal.java
+++ tcs/sumOfDiagonal.java
@@ -17,7 +17,7 @@ public class sumOfDiagonal {
                 arr[i][j]=sc.nextInt();
             }
          }
-         //com
+         //
 
          int sum =0;
          for (int i = 0; i < k; i++) {
